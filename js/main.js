4afe22b38f1bc82764e149134a4b19d242764e21
juvelist <39278128+juvelist@users.noreply.github.com>
2019-02-24 01:33:25 +0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,3 +2,23 @@ $('.print__btn').click(function (event) {
     window.print();
     event.preventDefault();
 });
+
+$('.right-block__skills-percent').each(function()
+{
+    var val = $(this).data('label');
+    $(this).css('width', val);
+
+});
+
+
+$('.right-block__skills-text').mouseover(function()
+{
+    var val = $(this).next('.right-block__skills-indicator').find('.right-block__skills-percent').data('label');
+    $(this).next('.right-block__skills-indicator').find('.right-block__skills-percent').css('width', '1');
+
+    $('.right-block__skills-text').mouseout(function()
+    {
+        $(this).next('.right-block__skills-indicator').find('.right-block__skills-percent').css('width', val);
+    });
+});
+
